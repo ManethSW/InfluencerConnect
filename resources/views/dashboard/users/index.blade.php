6b207662d0a7b1a43607bd448efde3e301517d95
Maneth Weerasinghe <manethweerasinghe@gmail.com>
2023-10-06 23:57:49 +0530

    minor changes

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -128,11 +128,6 @@
                         @endforeach
                     </tbody>
                 </table>
-                @if ($errors->any())
-                    @foreach ($errors->all() as $error)
-                        <div class="alert alert-danger">{{ $error }}</div>
-                    @endforeach
-                @endif
             </div>
         </div>
     </div>
